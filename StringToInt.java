7f7d23acf970776c6d445ab36ec1334bee0fbf58
Kevin Liu <kevinliu52805@gmail.com>
2021-12-21 11:43:37 -0500

    made the Stringto int function(no testing

--- StringToInt.java
+++ StringToInt.java
@@ -1,4 +1,18 @@
 public class StringToInt{
+  public static int stringToInt(String s){
+    int ans = 0;
+     if ((s.substring(0, 1)).equals("-")){
+       boolean neg = true;
+       s = s.substring(1);
+     }
+     for(int x = 0; x < s.length(); x++){
+       ans += valueOfDigit(s.charAt(x) * (Math.pow(10, x)));
+     }
+     if(neg){
+       ans = ans * -1;
+     }
+     return ans;
+   }
   public static int valueOfDigit(char c){
     return ((int)(c)) - 48;
   }
